7d2b03904c6fd443dc7b6f961949366edc95657b
John Olamide Alokan <alokanolamide@gmail.com>
2023-07-20 14:29:26 +0100

    [FIXED] sort.h

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort_helper(int *array, int lo, int hi, size_t size);
+int lomuto_partition(int *array, int lo, int hi, size_t size);
+void swap(int *a, int *b);
 
 #endif /* SORT_H_ */
